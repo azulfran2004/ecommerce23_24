55d999bccb9e4fed1a34ee0767179afad7cf5720
azulfran2004 <franciscobartolomegarcia@gmail.com>
2024-02-27 20:06:14 +0100

    ejercicio4 y 5 a medio

--- app/Http/Livewire/Admin/ShowProducts.php
+++ app/Http/Livewire/Admin/ShowProducts.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Order;
 use App\Models\Product;
 use Livewire\Component;
 use Livewire\WithPagination;
+
 
 class ShowProducts extends Component
 {
     use WithPagination;
 
     public $search;
+    public $ordenar = 'products.id';
+    public $direccionOrden = 'desc';
+
+
 
     public function updatingSearch()
     {
@@ -19,8 +25,34 @@ class ShowProducts extends Component
 
     public function render()
     {
-        $products = Product::where('name', 'LIKE', "%{$this->search}%")->paginate(10);
+        $ordenar = $this->ordenar;
+        $direccionOrden = $this->direccionOrden;
 
-        return view('livewire.admin.show-products', compact('products'))->layout('layouts.admin');
+        
+        $products = Product::where('name', 'LIKE', "%{$this->search}%");
+        
+        $products = $products->orderBy("$ordenar", $direccionOrden)
+        ->paginate(10);        
+        $orders= Order::all();
+
+        return view('livewire.admin.show-products', compact('products','orders'))->layout('layouts.admin');
+    }
+
+
+    public function ordenarPorVendidos()
+    {
+        $this->cambiarDireccionOrden();
+        $this->ordenar = 'products.name';
+    }
+    public function ordenarPorPendientes()
+    {
+        $this->cambiarDireccionOrden();
+        $this->ordenar = 'products.quantity';
+    }
+
+
+    private function cambiarDireccionOrden()
+    {
+        $this->direccionOrden = ($this->direccionOrden === 'asc') ? 'desc' : 'asc';
     }
 }
